05550a3c86be7988be2d99cbaf1c03212f5c50d2
Alexandr Biryukov <biralan.mj@gmail.com>
2020-11-08 22:31:04 +0500

    feat: creat todoList opp |script.js

---
+++ js/script.js
@@ -0,0 +1,112 @@
+'use strict';
+
+class Todo {
+  constructor(form, input, todoList, todoCompleted) {
+    this.form = document.querySelector(form);
+    this.input = document.querySelector(input);
+    this.todoList = document.querySelector(todoList);
+    this.todoCompleted = document.querySelector(todoCompleted);
+    this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+  }
+
+  addToStorage() {
+    localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
+  }
+
+  render() {
+    this.todoList.textContent = '';
+    this.todoCompleted.textContent = '';
+
+    this.todoData.forEach(this.createItem, this);
+    this.addToStorage();
+  }
+
+  createItem(todo) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.key = todo.key;
+    li.insertAdjacentHTML(
+      'beforeend',
+      `
+          <span class="text-todo">${todo.value}</span>
+          <div class="todo-buttons">
+            <button class="todo-remove"></button>
+            <button class="todo-complete"></button>
+          </div>
+    `
+    );
+
+    if (todo.completed) {
+      this.todoCompleted.append(li);
+    } else {
+      this.todoList.append(li);
+    }
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    if (this.input.value.trim()) {
+      const newTodo = {
+        value: this.input.value,
+        completed: false,
+        key: this.generateKey(),
+      };
+      this.todoData.set(newTodo.key, newTodo);
+      this.input.value = '';
+      this.render();
+    }
+  }
+
+  generateKey() {
+    return (
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15)
+    );
+  }
+
+  deleteItem(item) {
+    const [key] = [...this.todoData.entries()].find(
+      ([, value]) => value === item
+    );
+    this.todoData.delete(key);
+    this.render();
+  }
+
+  completedItem(item) {
+    item.completed = !item.completed;
+    this.render();
+  }
+
+  handler() {
+    //делегирование кнопки
+    const todoContainer = document.querySelector('.todo-container');
+    todoContainer.addEventListener('click', (event) => {
+      if (event.target.matches('.todo-complete')) {
+        const todoItemLi = event.target.closest('.todo-item');
+        const todoItem = this.todoData.get(todoItemLi.key);
+        this.completedItem(todoItem);
+        this.render();
+      }
+      if (event.target.matches('.todo-remove')) {
+        const todoItemLi = event.target.closest('.todo-item');
+        const todoItem = this.todoData.get(todoItemLi.key);
+        this.deleteItem(todoItem);
+      }
+    });
+  }
+
+  init() {
+    this.form.addEventListener('submit', this.addTodo.bind(this));
+    this.render();
+    this.handler();
+  }
+}
+
+const todo = new Todo(
+  '.todo-control',
+  '.header-input',
+  '.todo-list',
+  '.todo-completed'
+);
+
+todo.init();
